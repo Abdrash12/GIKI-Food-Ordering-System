c508540a11822a50690565420857784ad0c73c3b
Rashid <rsmaabdullah1@gmail.com>
2025-12-01 16:30:10 +0500

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react-swc';
 import path from 'path';
 
 export default defineConfig({
-  base: "/GIKI-Food-Ordering-System/",   // REQUIRED FOR GITHUB PAGES
+  base: "/",   
 
   plugins: [react()],
   resolve: {
